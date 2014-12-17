b80b49bc4a91dae26ba7e251f971a5c7c7541844
Yi-Lin Tsai <tsaiian@gmail.com>
2014-12-17 15:23:37 +0800

    First version

---
+++ K-means/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using TextClustering.Lib;
+using select_review_to_label.k_Means;
+
+namespace KMean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            K_Means kmeans = new K_Means(2);
+
+            kmeans.addDoc("平價牛排 淡水好店 吃吃喝喝 情侶約會 中價位 生日聚會 平價 節日聚會 家庭聚會 朋友聚會 吃到飽 異國料理 美式料理");
+            kmeans.addDoc("平價 板橋美食 食記 小吃 單點式 麻辣鴨血 重慶路 涼麵 麻辣 鴨血 情侶約會 吃吃喝喝 涼皮 小吃店");
+            kmeans.addDoc("客家美食 朋友聚會 客家菜 家庭聚會 苗栗 平價 預約 中價位 鶴山飯館 春酒 老店 單點式 免服務費 可包場 有包廂");
+            kmeans.addDoc("下午茶 平價 免服務費 朋友聚會 景觀咖啡 巷弄隱密 家庭聚會 情侶約會 中價位 可帶寵物 無菜單 複合式 咖啡簡餐");
+            kmeans.addDoc("台中泰式餐廳 中價位 泰式料理 月亮蝦餅 要預訂 單點式 魔幻廚房 家庭聚會 吃吃喝喝 朋友聚會 免服務費 節日聚會 大份量 泰式台菜 生日聚會");
+            kmeans.addDoc("朋友聚會 中價位 吃吃喝喝 家庭聚會 單點式 印度料理 烤餅 平價 精緻料理 咖哩 可刷卡 生日聚會 異國料理 海尼根Light精選店家");
+            kmeans.addDoc("婚宴 平價 家庭聚會 吃吃喝喝 單點式 可包場 有包廂 老店 主題特色餐廳 景觀餐廳");
+            kmeans.addDoc("吃到飽 下午茶 有服務費 中價位 朋友聚會 巷弄隱密 情侶約會 家庭聚會 節日聚會 生日聚會 Buffet 複合式 複合式料理 2008新開店");
+            kmeans.addDoc("情侶約會 法國料理 高價位 精緻料理 可刷卡 無菜單 節日聚會 家庭聚會 老店 吃吃喝喝 馬卡龍 朋友聚會 中價位 異國料理 法式料理");
+            kmeans.addDoc("家庭聚會 烏龍麵 吃吃喝喝 單點式 贊岐烏龍麵 情侶約會 平價 朋友聚會 精緻料理 台南美食 贊岐廚房 日本料理 日式定食 台南餐廳 中價位");
+
+            kmeans.train();
+
+            //kmeans.getResult();
+
+            kmeans.showResult();
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ K-means/k-Means/K-Means.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TextClustering.Lib;
+
+namespace select_review_to_label.k_Means
+{
+    /// <summary>
+    /// 整個K-means的介面
+    /// </summary>
+    class K_Means
+    {
+        private int txtClusterNum;
+        private DocumentCollection docCollection;
+        private List<Centroid> resultSet;
+        private int totalIteration = 0;
+
+        public K_Means()
+        {
+            docCollection = new DocumentCollection() { DocumentList = new List<string>() };
+        }
+
+        public K_Means(int num)
+        {
+            docCollection = new DocumentCollection() { DocumentList = new List<string>() };
+            txtClusterNum = num;
+        }
+
+        /// <summary>
+        /// 增加一篇文章進去
+        /// </summary>
+        /// <param name="article">一篇文章，用空格分開。</param>
+        public void addDoc(string article)
+        {
+            docCollection.DocumentList.Add(article);
+        }
+
+        /// <summary>
+        /// 開始訓練
+        /// </summary>
+        public void train()
+        {
+            List<DocumentVector> vSpace = VectorSpaceModel.ProcessDocumentCollection(docCollection);
+
+            totalIteration = 0;
+            resultSet = DocumnetClustering.PrepareDocumentCluster(txtClusterNum, vSpace, ref  totalIteration);
+
+
+            Console.WriteLine("totalIteration: " + totalIteration.ToString());
+        }
+
+        /// <summary>
+        /// 取得分群結果
+        /// </summary>
+        public List<List<string>> getResult()
+        {
+            if (resultSet == null)
+            {
+                Console.WriteLine("not train yet.");
+                return null;
+            }
+            else
+            {
+                string msg = string.Empty;
+
+
+                List<List<string>> result = new List<List<string>>();
+
+                foreach (Centroid c in resultSet)
+                {
+                    List<string> clusterContentList = new List<string>();
+
+                    foreach (DocumentVector document in c.GroupedDocument)
+                        clusterContentList.Add(document.Content);
+
+                    result.Add(clusterContentList);
+
+                }
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 直接印出分群結果
+        /// </summary>
+        public void showResult()
+        {
+            if (resultSet == null)
+            {
+                Console.WriteLine("not train yet.");
+                return;
+            }
+
+            string msg = string.Empty;
+            int count = 1;
+            foreach (Centroid c in resultSet)
+            {
+                msg += String.Format("------------------------------[ CLUSTER {0} ]-----------------------------{1}", count, System.Environment.NewLine);
+                foreach (DocumentVector document in c.GroupedDocument)
+                {
+                    //msg += "Doc ID: " + docIdDic.ge
+                    msg += document.Content + System.Environment.NewLine;
+                    if (c.GroupedDocument.Count > 1)
+                        msg += String.Format("{0}--------------------------------------------------------------------------{0}", System.Environment.NewLine);
+                }
+                count++;
+            }
+
+            Console.WriteLine(msg);
+        }
+
+        /// <summary>
+        /// 共跑了幾個Iteration
+        /// </summary>
+        public int getTotalIteration()
+        {
+            return totalIteration;
+        }
+        
+    }
+}
